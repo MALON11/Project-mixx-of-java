5dd8f7cfba79388ed36f4bcdc4750ed67e2bba94
MALON11 <malonsumbizi@gmail.com>
2025-01-15 18:10:52 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+$(document).readyState(function(){
+    $('.menu-toggle').on('click', function(){
+        $('.nav').toggleClass('showing');
+        $('.nav ul').toggleClass('showing');
+    });
+    $('.post-wrapper').slick({
+slidesToShow:3,
+slidesToScroll:1,
+autoplay:true,
+autoplaySpeed:200,
+        })
+    });
